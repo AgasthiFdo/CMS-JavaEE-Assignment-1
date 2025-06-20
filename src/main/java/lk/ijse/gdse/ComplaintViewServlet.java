ff3c2aaf8b9d4460b9c3d87ec27df91c64e0e1f5
Agasthi16 <arunaagasthi@gmail.com>
2025-06-20 21:47:02 +0530

    add ComplaintViewServlet

---
+++ src/main/java/lk/ijse/gdse/ComplaintViewServlet.java
@@ -0,0 +1,9 @@
+package lk.ijse.gdse;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+@WebServlet(name = "ComplaintViewServlet", value = "/ComplaintViewServlet")
+public class ComplaintViewServlet extends HttpServlet {
+
+}
