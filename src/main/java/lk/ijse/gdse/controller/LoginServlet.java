2f5ace799cd89c3a68b1a067e9575af0e45d912b
Agasthi16 <arunaagasthi@gmail.com>
2025-06-20 22:48:15 +0530

    update LoginServlet

--- src/main/java/lk/ijse/gdse/controller/LoginServlet.java
+++ src/main/java/lk/ijse/gdse/controller/LoginServlet.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
+import lk.ijse.gdse.dao.LoginDAO;
+import lk.ijse.gdse.dao.impl.LoginDAOImpl;
+import lk.ijse.gdse.dto.UserDTO;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import java.io.IOException;
@@ -22,7 +25,32 @@ public class LoginServlet extends HttpServlet {
         String password = req.getParameter("password");
 
 
+        LoginDAO loginDAO = new LoginDAOImpl(bds);
 
+        try {
+            UserDTO user = loginDAO.Login(name, password);
+
+            if (user != null) {
+                HttpSession session = req.getSession();
+                session.setAttribute("user", user);
+
+                String role = user.getRole();
+                if(role.equalsIgnoreCase("admin")) {
+                    session.setAttribute("admin", user);
+                   session.setAttribute("isAdmin", true);
+                    resp.sendRedirect("Admin");
+                }else {
+                    session.setAttribute("user", user);
+                    resp.sendRedirect("ComplaintServlet");
+                    //resp.sendRedirect("UserDashboard.jsp");
+
+                }
+            }else {
+                resp.sendRedirect("login.jsp?error=true");
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
