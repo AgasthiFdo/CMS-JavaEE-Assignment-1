36ff0670639da6639991bfdd491185dc21ccec0a
Agasthi16 <arunaagasthi@gmail.com>
2025-06-20 22:50:06 +0530

    add ComplaintDTO

---
+++ src/main/java/lk/ijse/gdse/dto/ComplaintDTO.java
@@ -0,0 +1,23 @@
+package lk.ijse.gdse.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ComplaintDTO {
+    private String id;
+    private String uid;
+    private String userName;
+    private String title;
+    private String Complaint;
+    private String date;
+    private String status;
+    private String remark;
+
+
+
+}
