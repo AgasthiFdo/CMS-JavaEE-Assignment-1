3bc683ab56db6b865279c53ab0edffc303fcb13a
Agasthi16 <arunaagasthi@gmail.com>
2025-06-20 22:09:32 +0530

    add ViewComplaintServlet

---
+++ src/main/java/lk/ijse/gdse/controller/ConnectionPool.java
@@ -0,0 +1,37 @@
+package lk.ijse.gdse.controller;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+import jakarta.servlet.annotation.WebListener;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.SQLException;
+
+@WebListener
+public class ConnectionPool implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        BasicDataSource basicDataSource = new BasicDataSource();
+
+        basicDataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        basicDataSource.setUrl("jdbc:mysql://localhost:3306/CMS");
+        basicDataSource.setUsername("root");
+        basicDataSource.setPassword("Ijse@1234");
+        basicDataSource.setInitialSize(50);
+        basicDataSource.setMaxTotal(50);
+
+        ServletContext servletContext = sce.getServletContext();
+        servletContext.setAttribute("dataSource", basicDataSource);
+
+    }
+    public void contextDestroyed(ServletContextEvent sce) {
+        try{
+            ServletContext servletContext = sce.getServletContext();
+            BasicDataSource basicDataSource = (BasicDataSource) servletContext.getAttribute("dataSource");
+            basicDataSource.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
